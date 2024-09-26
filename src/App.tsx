52d985f34f29c412f0267fa578248f0360d8a9f0
Tim Darrow <timothy.darrow7@gmail.com>
2024-09-25 22:35:22 -0700

    completed initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "./output.css";
 
 function App() {
   return (
